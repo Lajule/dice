5c3d1518e412ce4a92eb281a2606c533bdbfc2da
Julien Rouzieres <julien.rouzieres@mac.com>
2018-04-24 13:49:34 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
 	"regexp"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -18,9 +22,48 @@ func init() {
 
 func main() {
 	flag.Parse()
-	rand.Seed(time.Now().UTC().UnixNano())
-	fmt.Printf("%d\n", rand.Intn(6))
 
-	r, _ := regexp.Compile("^([0-9]+)d([0-9]+)(([+-])([0-9])+)?$")
-	fmt.Println(r.FindStringSubmatch("3d3+2"))
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	r, _ := regexp.Compile("^([0-9]+)d([0-9]+)([+-][0-9]+)?$")
+
+	scanner := bufio.NewScanner(strings.NewReader("3d32\n3d32+3\n3d\n5d6+3\n10d3-3"))
+
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		fmt.Printf("%s\n", text)
+
+		if r.MatchString(text) {
+			groups := r.FindStringSubmatch(text)
+
+			n, _ := strconv.Atoi(groups[1])
+			d, _ := strconv.Atoi(groups[2])
+
+			var draws []int
+
+			for i := 0; i < n; i++ {
+				r := rand.Intn(d)
+				draws = append(draws, r)
+			}
+
+			fmt.Printf("%v\n", draws)
+
+			sum := 0
+
+			for _, draw := range draws {
+				sum += draw
+			}
+
+			fmt.Printf("%d\n", sum)
+
+			a, _ := strconv.Atoi(groups[3])
+			fmt.Printf("%d\n", sum+a)
+
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
